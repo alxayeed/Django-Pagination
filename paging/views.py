39aa618ae44f55c1b0e6b665916f41d1313b64e9
alxayeed <alxayeed@gmail.com>
2020-09-11 20:49:08 +0600

    pagination added to the view

--- paging/views.py
+++ paging/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Post
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 
 def home(request):
-    # return HttpResponse('<h1 style="text-align:center">Home Page</h1>')
     all_posts = Post.objects.all()
-    context = {'all_posts': all_posts}
+
+    # creating Paginator object
+    paginator = Paginator(all_posts, 5, 3)
+    # page = paginator.get_page()
+    page_number = request.GET.get('page')
+    # print(request.GET.get('page', 1))
+    # print(request.GET.get.__doc__)
+    print(page_number)
+    posts = paginator.page(page_number)
+
+    context = {'posts': posts}
     return render(request, 'paging/home.html', context)
