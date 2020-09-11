b2b28bff45081df9813db718c6c5d0b33ea3074e
alxayeed <alxayeed@gmail.com>
2020-09-11 15:36:40 +0600

    superuser created.new model created

--- pagination/settings.py
+++ pagination/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'paging.apps.PagingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- paging/models.py
+++ paging/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=20)
+    body = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
